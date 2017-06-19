f2f616267534610d9adfe6466da500f9310525ec
anrosant <anrosant@espol.edu.ec>
2017-06-19 09:28:20 -0500

    archivo js JSON

---
+++ appJSON.js
@@ -0,0 +1,58 @@
+var http_request = false;
+
+function makeRequest(url) {
+
+
+    http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/JSON');
+            // Ver nota sobre esta linea al final
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    http_request.onreadystatechange = alertContents;
+    http_request.open('GET', url, true);
+    http_request.send(null);
+
+}
+
+function alertContents() {
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            /*Aquí deben procesar el JSON y mostrar la respuesta en el HTML*/
+            var ul=document.getElementById('lista-canciones');
+            var jsondoc = http_request.response;
+            var jsonResponse = JSON.parse(jsondoc);
+            for (var i = 0; i < jsonResponse.length; i++) {
+                var li=document.createElement('li');
+                var cancion= document.createTextNode(jsonResponse[i]["titulo"]);
+                li.appendChild(cancion);
+                ul.appendChild(li);
+            }
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+}
+
+window.onload = function() {
+    var link = document.getElementById('requerimiento');
+    link.onclick = function() {
+        makeRequest('datos.json');
+    }
+}
